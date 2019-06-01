c1a13c3b6c2fc867383d4376e43e3d00e25dd590
Andrei Vasile <andrei.vasile97@gmail.com>
2019-06-02 00:33:56 +0300

    add protobuf as serialization method

--- client.js
+++ client.js
@@ -1,11 +1,26 @@
 const WebSocket = require('ws');
-
+const TestMessage = require('./testmessage_pb');
 const ws = new WebSocket('ws://localhost:7070');
-
+ws.binaryType = 'arraybuffer'
+var message = new proto.TestMessage();
+const NS_PER_SEC = 1e9;
+const MS_PER_NS = 1e-6;
+var time;
 ws.on('open', function open() {
-    ws.send('something');
+    time = process.hrtime();
+
+    message.setSometext('Hello Protocol Buffers');
+    var bytes = message.serializeBinary();
+    ws.send(bytes);
+
+    // var obj = {
+    //     SomeText: "Hello Protocol Buffers"
+    // }
+    // ws.send(JSON.stringify(obj));
 });
 
 ws.on('message', function incoming(data) {
+    const diff = process.hrtime(time);
+    console.log(`Benchmark took ${ (diff[0] * NS_PER_SEC + diff[1])  * MS_PER_NS } milliseconds`);
     console.log(data);
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-
+var protobuf = require("protobufjs");
 const app = express();
 
 const server = http.createServer(app);
@@ -10,21 +10,32 @@ const wss = new WebSocket.Server({
     server
 });
 
-wss.on('connection', ws => {
+protobuf.load("testmessage.proto")
+    .then(root => {
+        var TestMessage = root.lookupType("TestMessage");
+        wss.on('connection', ws => {
 
-    //connection is up, let's add a simple simple event
-    ws.on('message', message => {
+            //connection is up, let's add a simple simple event
+            ws.on('message', data => {
 
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+                // Proto
+                var message = TestMessage.decode(data);
+                console.log('received: %s', message.someText);
+                ws.send(`Hello, you sent -> ${message.someText}`);
+
+
+                // JSON
+
+                // var message = JSON.parse(data);
+                // console.log('received: %s', message.SomeText);
+                // ws.send(`Hello, you sent -> ${message.SomeText}`);
+
+
+            });
+        });
+
+        //start our server
+        server.listen(process.env.PORT || 7070, () => {
+            console.log(`Server started on port ${server.address().port} :)`);
+        });
     });
-
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
-});
-
-//start our server
-server.listen(process.env.PORT || 7070, () => {
-    console.log(`Server started on port ${server.address().port} :)`);
-});
